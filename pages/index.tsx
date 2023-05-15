c3c9799aabc59d817d6367b78a42779bd51bb86f
jwalus <walusjacob@gmail.com>
2023-05-15 17:53:56 -0500

    first commit and some file structure

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Twitter</title>
+      </Head>
+      <div>
+        <h1 className='text-white'>
+          Hello, World!
+        </h1>
+      </div>
+    </>
+  )
+}
