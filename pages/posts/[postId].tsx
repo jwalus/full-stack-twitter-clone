fdb85f6c97e1a5529e08e846ff0146021b76efb3
jwalus <walusjacob@gmail.com>
2023-05-21 19:02:03 -0500

    comments working and showing

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -4,6 +4,7 @@ import { ClipLoader } from "react-spinners";
 import Header from "../../components/Header";
 import PostItem from "../../components/posts/PostItem";
 import Form from "../../components/Form";
+import CommentFeed from "../../components/posts/CommentFeed";
 
 const PostView = () => {
   const router = useRouter();
@@ -23,11 +24,13 @@ const PostView = () => {
     <>
       <Header label="Tweet" showBackArrow />
       <PostItem data={fetchedPost} />
-      <Form 
+      <div className="pt-3" />
+      <Form
       postId={postId as string}
       isComment 
       placeholder="Tweet your reply"
       />
+      <CommentFeed comments={fetchedPost?.comments} />
     </>
   )
 }
